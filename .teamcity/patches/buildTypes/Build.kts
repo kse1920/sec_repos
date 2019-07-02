3f826b02a71a9d5a92112d4994c06ccd8499cdc8
kse1920 <kse1920@TeamCity>
2019-07-02 13:48:36 +0300

    TeamCity change in '59129-2' project: runners of 'BuildChange' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,6 +1,9 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.ideaInspections
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -13,4 +16,31 @@ changeBuildType(RelativeId("Build")) {
         "Unexpected name: '$name'"
     }
     name = "BuildChange"
+
+    expectSteps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        ideaInspections {
+            pathToProject = "pom.xml"
+            jvmArgs = "-Xmx512m -XX:ReservedCodeCacheSize=240m"
+            targetJdkHome = "%env.JDK_18%"
+        }
+        script {
+            scriptContent = """echo "hello""""
+        }
+    }
+    steps {
+        insert(1) {
+            script {
+                scriptContent = """echo "hello""""
+            }
+        }
+        items.removeAt(3)
+        check(stepsOrder == arrayListOf("RUNNER_1", "RUNNER_2", "RUNNER_3")) {
+            "Unexpected build steps order: $stepsOrder"
+        }
+        stepsOrder = arrayListOf<String>()
+    }
 }
