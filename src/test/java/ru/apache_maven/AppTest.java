3f375b9525807a5e1255663df11c1afef13e18ce
Mona Lisa <kse1920@yandex.ru>
2019-08-14 16:35:21 +0300

    add new tests

--- src/test/java/ru/apache_maven/AppTest.java
+++ src/test/java/ru/apache_maven/AppTest.java
@@ -18,11 +18,13 @@ public class AppTest extends TestCase
     /**
      * Rigorous Test :-)
      */
+    //@Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-    }	
-	public AppTest( String testName ){
+    }
+
+    public AppTest( String testName ){
         super( testName );
     }
 
@@ -31,43 +33,31 @@ public class AppTest extends TestCase
     }
 
     public void testApp(){
-        assertTrue( false );
-    }
-    
-    public void testFailed2(){
-        assertTrue( false );
-    }
-    
-    public void testFailed3(){
-        assertTrue( false );
-    }
-    
-    public void testFailed4(){
-        assertTrue( false );
-    }
-
-    public void testFailed5(){
-        assertTrue( false );
-    }
-    
-    public void testFailed6(){
-        assertTrue( false );
-    }
-
-    public void testFailed7(){
         assertTrue( true );
     }
-    
-    public void testFailed8(){
-        assertTrue( false );
-    }    
-   public void testAssert() {
-		 String message = new AppBean("Maven 2").sayHello();
-         Assert.assertEquals("Test Hello Machine", "Hello Maven 2", message);
+
+    public void testAssert() {
+        String message = new AppBean("Maven 2").sayHello();
+        Assert.assertEquals("Test Hello Machine", "Hello Maven 2", message);
     }
-    
-  public void testAssert2() {
-		 String message2 = "Test Sec";
-         Assert.assertEquals("Second Test", "Test Sec", message2);
+
+    public void testAssert2() {
+        String message2 = "Test Sec2";
+        Assert.assertEquals("Second Test", "Test Sec2", message2);
+    }
+
+    public void testAssert3() {
+        String message3 = "Test Sec3";
+        Assert.assertEquals("Second Test", "Test Sec", message3);
+    }
+
+    public void testAssert4() {
+        String message4 = "Test Sec4";
+        Assert.assertEquals("Second Test", "Test Sec", message4);
+    }
+
+    public void testAssert5() {
+        String message5 = "Test Sec5";
+        Assert.assertEquals("Second Test", "Test Sec5", message5);
     }
 }
