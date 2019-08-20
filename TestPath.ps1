6d5258b12668bdb7a2207bd480acbdb68785d05f
ksenia.ilina <44381451+kse1920@users.noreply.github.com>
2019-08-20 12:20:28 +0300

    Delete TestPath.ps1

--- TestPath.ps1
+++
@@ -1,7 +0,0 @@
-Function TestPath
-        {
-                PARAM (
-                [PARAMETER(Mandatory=$True,Position=0,HelpMessage = "Путь до проверяемого ресурса",ParameterSetName='Path')]$Path
-                )
-                Return(Test-Path $Path)
-        }
