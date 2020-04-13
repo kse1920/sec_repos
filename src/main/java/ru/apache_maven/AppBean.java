85695cb6a36dbed4c72456d02b3db6027f9e7a1a
kseniailina <kse1920@yandex.ru>
2020-04-13 11:28:20 +0300

    Default Changelist

--- src/main/java/ru/apache_maven/AppBean.java
+++ src/main/java/ru/apache_maven/AppBean.java
@@ -1,12 +1,11 @@
 package ru.apache_maven;
 
-public class AppBean 
+public class AppBean
 {
-	private String text; 
+    private String text;
     public AppBean(final String text) {
         this.text = text;
     }
-
     public String sayHello() {
         return "Hello " + text;
     }
